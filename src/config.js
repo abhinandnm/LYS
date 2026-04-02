9c5cddf2d5ab1cfdce45b326b9cb0f51b56abbd7
Abhinand N M <abhinandnm1@gmail.com>
2026-04-03 01:44:38 +0530

    Update: Set API_URL to live EC2 Public IP (13.235.13.0)

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 // Replace 'YOUR_EC2_PUBLIC_IP' with the actual IP address from your AWS Console
 
 const CONFIG = {
-  API_URL: 'http://YOUR_EC2_PUBLIC_IP:3001' // Example: 'http://13.233.123.45:3001'
+  API_URL: 'http://13.235.13.0:3001'
 };
 
 export default CONFIG;
