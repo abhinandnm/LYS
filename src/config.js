6b1088af7540588dcadb0e97238028b2d9e53fad
Abhinand N M <abhinandnm1@gmail.com>
2026-04-03 12:02:19 +0530

    Configure API URL for Amplify Proxy bypass

--- src/config.js
+++ src/config.js
@@ -2,7 +2,9 @@
 // Replace 'YOUR_EC2_PUBLIC_IP' with the actual IP address from your AWS Console
 
 const CONFIG = {
-  API_URL: 'http://13.235.13.0:3001'
+  // If in production (Amplify), use relative path to seamlessly proxy through HTTPS
+  // If in local development, connect directly to proxy off HTTP
+  API_URL: import.meta.env.PROD ? '' : 'http://13.235.13.0:3001'
 };
 
 export default CONFIG;
