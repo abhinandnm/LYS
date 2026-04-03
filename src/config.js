665f6c8aa7bcb4d588844297805efef2b3e639ff
Abhinand N M <abhinandnm1@gmail.com>
2026-04-03 12:18:20 +0530

    Revert API to hardcoded IP

--- src/config.js
+++ src/config.js
@@ -2,9 +2,7 @@
 // Replace 'YOUR_EC2_PUBLIC_IP' with the actual IP address from your AWS Console
 
 const CONFIG = {
-  // If in production (Amplify), use relative path to seamlessly proxy through HTTPS
-  // If in local development, connect directly to proxy off HTTP
-  API_URL: import.meta.env.PROD ? '' : 'http://13.235.13.0:3001'
+  API_URL: 'http://13.235.13.0:3001'
 };
 
 export default CONFIG;
